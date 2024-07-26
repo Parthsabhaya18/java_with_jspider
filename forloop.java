26acc1768810ff41415a5102075bf1897dda5811
Parth Sabhaya <parthsabhaya0718@gmail.com>
2024-07-26 18:35:24 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop {
+    public static void main(String[] args) {
+        int n=50;
+        for(int i=1;i<=n;i=i+4)
+        {
+            System.out.println(i);
+        }
+    }
+}
